015dcbb49b346a1e92426cc5e1d2ee57938123ae
Peter Piper <petermpiper@gmail.com>
2020-09-24 09:47:03 -0400

    Coded the UIPanel class header and source file
    Coded the UIPanel class header and source file

---
+++ Space Invaders ++/UIPanel.cpp
@@ -0,0 +1,60 @@
+#include "UIPanel.h"
+
+UIPanel::UIPanel(Vector2i res, int x, int y, float width, float height, int alpha, int red, int green, int blue)
+{
+	m_UIPanel.setFillColor(sf::Color(red, green, blue, alpha));
+
+	// How big in pixels is the UI panel
+	m_UIPanel.setSize(Vector2f(width, height));
+
+	// Where in pixels does the center of the view focus
+	// This is most releveant when drawing a portion
+	// of the game world
+	// width/2, height/2 ensures it is exactly centered around the 
+	// RectangleShape, m_UIPanel
+	m_View.setCenter(width / 2, height / 2);
+
+	// Where in the window is the view positioned?
+	float viewportStartX = 1.f / (res.x / x);
+	float viewportStartY = 1.f / (res.y / y);
+	float viewportSizeX = 1.f / (res.x / width);
+	float ViewportSizeY = 1.f / (res.y / height);
+
+	// Params from left to right
+	// StartX as a fraction of 1, startY as a fraction of 1
+	// SizeX as a fraction of 1, sizeY as a fraction of 1
+	m_View.setViewport(FloatRect(viewportStartX, viewportStartY, viewportSizeX, ViewportSizeY));
+}
+
+vector<shared_ptr<Button>>UIPanel::getButtons()
+{
+	return m_Buttons;
+}
+
+void UIPanel::addButton(float x, float y, int width, int height, int red, int green, int blue, string label)
+{
+	m_Buttons.push_back(make_shared<Button>(Vector2f(x, y), width, height, red, green, blue, label));
+}
+
+void UIPanel::draw(RenderWindow& window)
+{
+	window.setView(m_View);
+	if (!m_Hidden)
+	{
+		window.draw(m_UIPanel);
+		for (auto it = m_Buttons.begin(); it != m_Buttons.end(); ++it)
+		{
+			(*it)->draw(window);
+		}
+	}
+}
+
+void UIPanel::show()
+{
+	m_Hidden = false;
+}
+
+void UIPanel::hide()
+{
+	m_Hidden = true;
+}
