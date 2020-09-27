3c521538f92a54e92eacfc263217fb935bbbce0c
Peter Piper <petermpiper@gmail.com>
2020-09-27 07:37:48 -0400

    Coded the GameOverInputHandler class header and source file
    Coded the GameOverInputHandler class header and source file

---
+++ Space Invaders ++/GameOverInputHandler.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "InputHandler.h"
+
+class GameOverInputHandler : public InputHandler
+{
+public:
+	void handleKeyPressed(Event& event, RenderWindow& window) override;
+
+	void handleLeftClick(std::string& buttonInteractedWith, RenderWindow& window) override;
+};
